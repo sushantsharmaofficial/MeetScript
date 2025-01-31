8a47b8ece806948c0b44af5dd797a7bb7469265b
Sushant Sharma <sushant.ku.sh.70@gmail.com>
2025-01-31 22:42:43 +0545

    chnaged in the project

--- src/app/(root)/(home)/page.tsx
+++ src/app/(root)/(home)/page.tsx
@@ -1,5 +1,80 @@
-import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+"use client";
+
+import { QUICK_ACTIONS } from "@/constants";
+import { useUserRole } from "@/hooks/useUserRole";
+import { action } from "../../../../convex/_generated/server";
+import ActionCard from "@/components/ui/ActionCard";
+import { useState } from "react";
+import { useQuery } from "convex/react";
+import { api } from "../../../../convex/_generated/api";
+import { useRouter } from "next/navigation";
+import MeetingModel from "@/components/MeetingModel";
 
 export default function Home() {
-  return <div>Home</div>;
+  // router
+  const router = useRouter();
+
+  const { isInterviewer, isCandidate, isLoading } = useUserRole();
+  const interviews = useQuery(api.interviews.getMyInterviews);
+
+  const [showModel, setShowModel] = useState(false);
+  const [modelType, setModelType] = useState<"start" | "join">();
+
+  const handleQuickAction = (title: string) => {
+    switch (title) {
+      case "New Call":
+        setModelType("start");
+        setShowModel(true);
+        break;
+      case "Join Interview":
+        setModelType("join");
+        setShowModel(true);
+        break;
+      default:
+        router.push(`/${title.toLowerCase()}`);
+        break;
+    }
+  };
+
+  if (isLoading) return <div>Loading...</div>;
+  return (
+    <div className=" container max-w-7xl mx-auto p-6">
+      {/* WELCOME SECTION */}
+      <div className="rounded-lg bg-card p-6 border shadow-sm mb-10">
+        <h1 className="text-4xl font-bold bg-gradient-to-r from-rose-600 to-rose-500 bg-clip-text text-transparent">
+          Welcome back!
+        </h1>
+        <p className="text-muted-foreground mt-2">
+          {isInterviewer
+            ? "Manage your interviews and review candidates effectively"
+            : "Access your upcoming interviews and preparations"}
+        </p>
+      </div>
+
+      {isInterviewer ? (
+        <>
+          <div className=" grid sm:grid-cols-2 lg:grid-cols-4 gap-6">
+            {QUICK_ACTIONS.map((action, index) => (
+              <ActionCard
+                key={action.title}
+                action={action}
+                onClick={() => handleQuickAction(action.title)}
+              />
+            ))}
+          </div>
+
+          <MeetingModel
+            isOpen={showModel}
+            onClose={() => setShowModel(false)}
+            title={modelType === "join" ? "Join Meeting" : "Start Meeting"}
+            isJoinMeeting={modelType === "join"}
+          />
+        </>
+      ) : (
+        <>
+          <div className=" ">{/* candidate views goes here */}</div>
+        </>
+      )}
+    </div>
+  );
 }
---
+++ src/components/MeetingModel.tsx
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+import { Dialog, DialogContent, DialogHeader, DialogTitle } from "./ui/dialog";
+import { Input } from "./ui/input";
+import { Button } from "./ui/button";
+import useMeetingActions from "@/hooks/useMeetingActions";
+
+interface MeetingModelProps {
+  isOpen: boolean;
+  onClose: () => void;
+  title: string;
+  isJoinMeeting: boolean;
+}
+
+function MeetingModel({
+  isOpen,
+  onClose,
+  title,
+  isJoinMeeting,
+}: MeetingModelProps) {
+  const [meetingUrl, setMeetingUrl] = useState("");
+
+  const { createInstantMeeting, joinMeeting } = useMeetingActions();
+
+  const handleStart = () => {};
+
+  return (
+    <Dialog open={isOpen} onOpenChange={onClose}>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>{title}</DialogTitle>
+        </DialogHeader>
+        <div className=" space-y-4 pt-4">
+          {isJoinMeeting && (
+            <Input
+              placeholder="Paste the meeting URL"
+              value={meetingUrl}
+              onChange={(e) => setMeetingUrl(e.target.value)}
+            />
+          )}
+          <div className=" flex justify-end gap-3">
+            <Button variant="outline" onClick={onClose}>
+              Cancel
+            </Button>
+            <Button
+              onClick={handleStart}
+              disabled={isJoinMeeting && !meetingUrl.trim()}
+            >
+              {isJoinMeeting ? "Join Meeting" : "Start Meeting"}
+            </Button>
+          </div>
+        </div>
+      </DialogContent>
+    </Dialog>
+  );
+}
+
+export default MeetingModel;
