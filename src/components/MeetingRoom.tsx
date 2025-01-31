db89cd9112898d35ade94c5ab8e74533a9e67e42
Sushant Sharma <sushant.ku.sh.70@gmail.com>
2025-01-31 23:39:29 +0545

    started settingup meetingroom

--- src/components/MeetingRoom.tsx
+++ src/components/MeetingRoom.tsx
@@ -1,6 +1,10 @@
-import React from "react";
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
 
 function MeetingRoom() {
+  const router = useRouter();
+  const [layout, setLayout] = useState<"grid" | "speaker">("speaker");
+  const [showParticipants, setShowParticipants] = useState(false);
   return <div>MeetingRoom</div>;
 }
 
