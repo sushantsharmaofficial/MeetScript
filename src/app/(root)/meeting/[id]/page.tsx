1fddde66cf839798035c2a7bb31df4e5c448ade3
Sushant Sharma <sushant.ku.sh.70@gmail.com>
2025-01-31 23:07:56 +0545

    meeting page

--- src/app/(root)/meeting/[id]/page.tsx
+++ src/app/(root)/meeting/[id]/page.tsx
@@ -1,6 +1,19 @@
-import React from "react";
+"use client";
+
+import LoaderUI from "@/components/LoaderUI";
+import { useUser } from "@clerk/nextjs";
+import { useParams } from "next/navigation";
+import React, { useState } from "react";
 
 function MeetingPage() {
+  const { id } = useParams();
+  const { isLoaded } = useUser();
+  const [isCallingLoading, setIsCallingLoading] = useState(true);
+
+  const [isSetupComplete, setIsSetupComplete] = useState(false);
+
+  if (!isLoaded || isCallingLoading) return <LoaderUI />;
+
   return <div>MeetingPage</div>;
 }
 
